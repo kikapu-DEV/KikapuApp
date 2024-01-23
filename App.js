4e8d4bc0016787c8e5e4ab47b5c9f1fa54ec145e
Diana-nyamai <dnyamai.dn@gmail.com>
2024-01-24 00:53:23 +0300

    Update: setting up react query

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { AuthProvider } from "./helpers/contexts/AuthContext";
 import AppRoutes from "./navigation/AppRoutes";
 import {Provider} from 'react-redux';
 import store from "./store";
+import {QueryClient, QueryClientProvider} from 'react-query'
 
+const queryClient = new QueryClient();
 export default function App() {
 	return (
+		<QueryClientProvider client={queryClient}>
 		<AuthProvider>
 			<Provider store={store}>
 				<AppRoutes />
 			</Provider>
 			
 		</AuthProvider>
+		</QueryClientProvider>
 	);
 }
