1616247d2b5fa72b6e5601c338b9fe7453b6d25e
Diana-nyamai <dnyamai.dn@gmail.com>
2024-01-24 00:29:43 +0300

    Update: setting up redux

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { AuthProvider } from "./helpers/contexts/AuthContext";
 import AppRoutes from "./navigation/AppRoutes";
+import {Provider} from 'react-redux';
+import store from "./store";
 
 export default function App() {
 	return (
 		<AuthProvider>
-			<AppRoutes />
+			<Provider store={store}>
+				<AppRoutes />
+			</Provider>
+			
 		</AuthProvider>
 	);
 }
---
+++ store/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({
+    reducers:{
+        // TODO: add the other slices here
+    }
+})
+
+export default rootReducer
