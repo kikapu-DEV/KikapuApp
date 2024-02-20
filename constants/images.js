97baf0b4ab853228a5b3845d74644dc0ed5d8c93
Diana-nyamai <dnyamai.dn@gmail.com>
2024-02-21 01:02:58 +0300

    update: add new screen

--- constants/images.js
+++ constants/images.js
@@ -1,91 +1,93 @@
-import kikapuLogoSmall from '../assets/kikapuLogoSmall.png';
-import foodBasket from '../assets/images/foodBasket.png';
-import kenyanFlag from '../assets/images/kenyanFlag.png';
-import avatar1 from '../assets/images/avatar1.png';
-import food1 from '../assets/images/food1.png'; 
-import food2 from '../assets/images/food2.png'; 
-import food3 from '../assets/images/food3.png'; 
-import available1 from '../assets/images/available1.png';
-import available2 from '../assets/images/available2.png';
-import kikapuLogo from '../assets/images/kikapuLogo.png';
-import smallAvatar from '../assets/images/smallAvatar.png';
-import success from '../assets/images/success.png';
-import avatar2 from '../assets/images/avatar2.png';
-import restFood from '../assets/images/restFood.png';
-import burger from '../assets/images/burger.png';
-import burger2 from '../assets/images/burger2.png';
-import cup from '../assets/images/cup.png';
-import apple from '../assets/images/apple.png';
-import vegetable from '../assets/images/vegetable.png';
-import cheese from '../assets/images/cheese.png';
-import meat from '../assets/images/meat.png';
-import hoho from '../assets/images/hoho.png';
-import lambMeat from '../assets/images/lambMeat.png';
-import arabGinger from '../assets/images/arabGinger.png';
-import lettuce from '../assets/images/lettuce.png';
-import butterNut from '../assets/images/butterNut.png';
-import carrots from '../assets/images/carrots.png';
-import broccoli from '../assets/images/broccoli.png';
-import tomatoes from '../assets/images/tomatoes.png';
-import Spinach from '../assets/images/Spinach.png';
-import arabGinger2 from '../assets/images/arabGinger2.png';
-import d1 from '../assets/images/d1.png';
-import d2 from '../assets/images/d2.png';
-import d3 from '../assets/images/d3.png';
-import d4 from '../assets/images/d4.png';
-import mpesa from '../assets/images/mpesa.png';
-import creditCard from '../assets/images/creditCard.png';
-import addCard from '../assets/images/addCard.png';
-import masterCard from '../assets/images/masterCard.png';
-import c1 from '../assets/images/restCart/c1.png';
-import c2 from '../assets/images/restCart/c2.png';
-import c3 from '../assets/images/restCart/c3.png';
-import c4 from '../assets/images/restCart/c4.png';
-import basket from '../assets/images/farmers/basket.png';
+import kikapuLogoSmall from "../assets/kikapuLogoSmall.png";
+import foodBasket from "../assets/images/foodBasket.png";
+import kenyanFlag from "../assets/images/kenyanFlag.png";
+import avatar1 from "../assets/images/avatar1.png";
+import food1 from "../assets/images/food1.png";
+import food2 from "../assets/images/food2.png";
+import food3 from "../assets/images/food3.png";
+import available1 from "../assets/images/available1.png";
+import available2 from "../assets/images/available2.png";
+import kikapuLogo from "../assets/images/kikapuLogo.png";
+import smallAvatar from "../assets/images/smallAvatar.png";
+import success from "../assets/images/success.png";
+import avatar2 from "../assets/images/avatar2.png";
+import restFood from "../assets/images/restFood.png";
+import burger from "../assets/images/burger.png";
+import burger2 from "../assets/images/burger2.png";
+import cup from "../assets/images/cup.png";
+import apple from "../assets/images/apple.png";
+import vegetable from "../assets/images/vegetable.png";
+import cheese from "../assets/images/cheese.png";
+import meat from "../assets/images/meat.png";
+import hoho from "../assets/images/hoho.png";
+import lambMeat from "../assets/images/lambMeat.png";
+import arabGinger from "../assets/images/arabGinger.png";
+import lettuce from "../assets/images/lettuce.png";
+import butterNut from "../assets/images/butterNut.png";
+import carrots from "../assets/images/carrots.png";
+import broccoli from "../assets/images/broccoli.png";
+import tomatoes from "../assets/images/tomatoes.png";
+import Spinach from "../assets/images/Spinach.png";
+import arabGinger2 from "../assets/images/arabGinger2.png";
+import d1 from "../assets/images/d1.png";
+import d2 from "../assets/images/d2.png";
+import d3 from "../assets/images/d3.png";
+import d4 from "../assets/images/d4.png";
+import mpesa from "../assets/images/mpesa.png";
+import creditCard from "../assets/images/creditCard.png";
+import addCard from "../assets/images/addCard.png";
+import masterCard from "../assets/images/masterCard.png";
+import c1 from "../assets/images/restCart/c1.png";
+import c2 from "../assets/images/restCart/c2.png";
+import c3 from "../assets/images/restCart/c3.png";
+import c4 from "../assets/images/restCart/c4.png";
+import basket from "../assets/images/farmers/basket.png";
+import fork from "../assets/images/fork.png";
 
 export default {
-    kikapuLogoSmall,
-    foodBasket,
-    kenyanFlag,
-    avatar1,
-    food1,
-    food2,
-    food3,
-    available1,
-    available2,
-    kikapuLogo,
-    smallAvatar,
-    success,
-    avatar2,
-    restFood,
-    burger,
-    burger2,
-    cup,
-    apple,
-    vegetable,
-    cheese,
-    meat,
-    hoho,
-    lambMeat,
-    arabGinger,
-    lettuce,
-    butterNut,
-    carrots,
-    broccoli,
-    tomatoes,
-    Spinach,
-    arabGinger2,
-    d1,
-    d2,
-    d3,
-    d4,
-    mpesa,
-    creditCard,
-    addCard,
-    masterCard,
-    c1,
-    c2,
-    c3,
-    c4,
-    basket,
+  kikapuLogoSmall,
+  foodBasket,
+  kenyanFlag,
+  avatar1,
+  food1,
+  food2,
+  food3,
+  available1,
+  available2,
+  kikapuLogo,
+  smallAvatar,
+  success,
+  avatar2,
+  restFood,
+  burger,
+  burger2,
+  cup,
+  apple,
+  vegetable,
+  cheese,
+  meat,
+  hoho,
+  lambMeat,
+  arabGinger,
+  lettuce,
+  butterNut,
+  carrots,
+  broccoli,
+  tomatoes,
+  Spinach,
+  arabGinger2,
+  d1,
+  d2,
+  d3,
+  d4,
+  mpesa,
+  creditCard,
+  addCard,
+  masterCard,
+  c1,
+  c2,
+  c3,
+  c4,
+  basket,
+  fork,
 };
